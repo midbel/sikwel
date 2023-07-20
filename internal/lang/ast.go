1a989cbec1ecff622d0d48d07df84f0f2c993d2c
midbel <brun.nicolas21@gmail.com>
2023-07-20 21:25:00 +0200

    parse savepoint and related statement

--- internal/lang/ast.go
+++ internal/lang/ast.go
@@ -14,6 +14,65 @@ type Expression interface {
 	// fmt.Stringer
 }
 
+type TransactionMode int
+
+const (
+	ModeReadWrite TransactionMode = 1 << (iota + 1)
+	ModeReadOnly
+)
+
+type TransactionLevel int
+
+const (
+	LevelReadRepeat TransactionLevel = 1 << (iota + 1)
+	LevelReadCommit
+	LevelReadUncommit
+	LevelSerializable
+)
+
+type SetTransaction struct {
+	Mode  TransactionMode
+	Level TransactionLevel
+}
+
+func (_ SetTransaction) Keyword() (string, error) {
+	return "SET TRANSACTION", nil
+}
+
+type StartTransaction struct {
+	Mode TransactionMode
+	Body Statement
+	End  Statement
+}
+
+func (_ StartTransaction) Keyword() (string, error) {
+	return "START TRANSACTION", nil
+}
+
+type Savepoint struct {
+	Name string
+}
+
+func (_ Savepoint) Keyword() (string, error) {
+	return "SAVEPOINT", nil
+}
+
+type ReleaseSavepoint struct {
+	Name string
+}
+
+func (_ ReleaseSavepoint) Keyword() (string, error) {
+	return "RELEASE SAVEPOINT", nil
+}
+
+type RollbackSavepoint struct {
+	Name string
+}
+
+func (_ RollbackSavepoint) Keyword() (string, error) {
+	return "ROLLBACK TO SAVEPOINT", nil
+}
+
 type Commit struct{}
 
 type Rollback struct{}
--- internal/lang/parser.go
+++ internal/lang/parser.go
@@ -53,12 +53,11 @@ func NewParserWithKeywords(r io.Reader, set KeywordSet) (*Parser, error) {
 	p.RegisterParseFunc("IF", p.parseIf)
 	p.RegisterParseFunc("CASE", p.parseCase)
 	p.RegisterParseFunc("WHILE", p.parseWhile)
-	p.RegisterParseFunc("COMMIT", p.parseCommit)
-	p.RegisterParseFunc("ROLLBACK", p.parseRollback)
 	p.RegisterParseFunc("DECLARE", p.parseDeclare)
 	p.RegisterParseFunc("SET", p.parseSet)
 	p.RegisterParseFunc("RETURN", p.parseReturn)
 	p.RegisterParseFunc("BEGIN", p.parseBegin)
+	p.RegisterParseFunc("START TRANSACTION", p.parseStartTransaction)
 
 	p.infix = make(map[symbol]infixFunc)
 	p.RegisterInfix("", Plus, p.parseInfixExpr)
@@ -392,6 +391,141 @@ func (p *Parser) parseBody(done func() bool) (Statement, error) {
 		return nil, p.Unexpected("body")
 	}
 	return list, nil
+}
+
+func (p *Parser) parseSetTransaction() (Statement, error) {
+	p.Next()
+	var (
+		stmt SetTransaction
+		err  error
+	)
+	if p.IsKeyword("ISOLATION LEVEL") {
+		p.Next()
+		switch {
+		case p.IsKeyword("REPEATABLE READ"):
+			stmt.Level = LevelReadRepeat
+		case p.IsKeyword("READ COMMITTED"):
+			stmt.Level = LevelReadCommit
+		case p.IsKeyword("READ UNCOMMITTED"):
+			stmt.Level = LevelReadUncommit
+		case p.IsKeyword("SERIALIZABLE"):
+			stmt.Level = LevelSerializable
+		default:
+			return nil, p.Unexpected("transaction")
+		}
+		p.Next()
+	}
+	switch {
+	case p.IsKeyword("READ ONLY"):
+		stmt.Mode = ModeReadOnly
+		p.Next()
+	case p.IsKeyword("READ WRITE"):
+		stmt.Mode = ModeReadWrite
+		p.Next()
+	case p.Is(EOL):
+	default:
+		return nil, p.Unexpected("transaction")
+	}
+	return stmt, err
+}
+
+func (p *Parser) parseStartTransaction() (Statement, error) {
+	defer func() {
+		p.UnregisterParseFunc("SAVEPOINT")
+		p.UnregisterParseFunc("COMMIT")
+		p.UnregisterParseFunc("ROLLBACK")
+		p.UnregisterParseFunc("RELEASE")
+		p.UnregisterParseFunc("RELEASE SAVEPOINT")
+		p.UnregisterParseFunc("ROLLBACK TO SAVEPOINT")
+		p.UnregisterParseFunc("SET TRANSACTION")
+	}()
+	p.Next()
+
+	var (
+		stmt StartTransaction
+		err  error
+	)
+	switch {
+	case p.IsKeyword("READ ONLY"):
+		stmt.Mode = ModeReadOnly
+		p.Next()
+	case p.IsKeyword("READ WRITE"):
+		stmt.Mode = ModeReadWrite
+		p.Next()
+	case p.Is(EOL):
+	default:
+		return nil, p.Unexpected("transaction")
+	}
+	if !p.Is(EOL) {
+		return nil, p.Unexpected("transaction")
+	}
+	p.Next()
+
+	p.RegisterParseFunc("SAVEPOINT", p.parseSavepoint)
+	p.RegisterParseFunc("RELEASE", p.parseReleaseSavepoint)
+	p.RegisterParseFunc("RELEASE SAVEPOINT", p.parseReleaseSavepoint)
+	p.RegisterParseFunc("ROLLBACK TO SAVEPOINT", p.parseRollbackSavepoint)
+	p.RegisterParseFunc("COMMIT", p.parseCommit)
+	p.RegisterParseFunc("ROLLBACK", p.parseRollback)
+	p.RegisterParseFunc("SET TRANSACTION", p.parseSetTransaction)
+
+	stmt.Body, err = p.parseBody(func() bool {
+		return p.IsKeyword("END") || p.IsKeyword("COMMIT") || p.IsKeyword("ROLLBACK")
+	})
+	if err != nil {
+		return nil, err
+	}
+	switch {
+	case p.IsKeyword("END") || p.IsKeyword("COMMIT"):
+		stmt.End = Commit{}
+	case p.IsKeyword("ROLLBACK"):
+		stmt.End = Rollback{}
+	default:
+		return nil, p.Unexpected("transaction")
+	}
+	p.Next()
+	return stmt, err
+}
+
+func (p *Parser) parseSavepoint() (Statement, error) {
+	p.Next()
+	var (
+		stmt Savepoint
+		err  error
+	)
+	if p.Is(Ident) {
+		stmt.Name = p.GetCurrLiteral()
+		p.Next()
+	}
+	return stmt, err
+}
+
+func (p *Parser) parseReleaseSavepoint() (Statement, error) {
+	p.Next()
+	var (
+		stmt ReleaseSavepoint
+		err  error
+	)
+	if !p.Is(Ident) {
+		return nil, p.Unexpected("release savepoint")
+	}
+	stmt.Name = p.GetCurrLiteral()
+	p.Next()
+	return stmt, err
+}
+
+func (p *Parser) parseRollbackSavepoint() (Statement, error) {
+	p.Next()
+	var (
+		stmt RollbackSavepoint
+		err  error
+	)
+	if !p.Is(Ident) {
+		return nil, p.Unexpected("rollback savepoint")
+	}
+	stmt.Name = p.GetCurrLiteral()
+	p.Next()
+	return stmt, err
 }
 
 func (p *Parser) parseCommit() (Statement, error) {
