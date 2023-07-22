97a12c55ff17e97dac0a2848966fdefec175d4c7
midbel <brun.nicolas21@gmail.com>
2023-07-22 20:31:03 +0200

    skeleton for the linter

--- internal/lang/lint.go
+++ internal/lang/lint.go
@@ -1,9 +1,68 @@
 package lang
 
 import (
+	"fmt"
 	"io"
 )
 
-func Lint(r io.Reader) []error {
+type Linter struct {
+	parser *Parser
+}
+
+func Lint(r io.Reader) (*Linter, error) {
+	p, err := NewParser(r)
+	if err != nil {
+		return nil, err
+	}
+	i := Linter{
+		parser: p,
+	}
+	return &i, nil
+}
+
+func (i *Linter) Lint() ([]error, error) {
+	stmt, err := i.parser.Parse()
+	if err != nil {
+		return nil, err
+	}
+	return i.LintStatement(stmt)
+}
+
+func (i *Linter) LintStatement(stmt Statement) ([]error, error) {
+	var errs []error
+	switch s := stmt.(type) {
+	case SelectStatement:
+		errs = i.lintSelect(s)
+	case InsertStatement:
+		errs = i.lintInsert(s)
+	case DeleteStatement:
+		errs = i.lintDelete(s)
+	case UpdateStatement:
+		errs = i.lintUpdate(s)
+	case WithStatement:
+		errs = i.lintWith(s)
+	default:
+		return nil, fmt.Errorf("unsupport statement type %T", stmt)
+	}
+	return errs, nil
+}
+
+func (i *Linter) lintSelect(stmt SelectStatement) []error {
+	return nil
+}
+
+func (i *Linter) lintInsert(stmt InsertStatement) []error {
+	return nil
+}
+
+func (i *Linter) lintUpdate(stmt UpdateStatement) []error {
+	return nil
+}
+
+func (i *Linter) lintDelete(stmt DeleteStatement) []error {
+	return nil
+}
+
+func (i *Linter) lintWith(stmt WithStatement) []error {
 	return nil
 }
