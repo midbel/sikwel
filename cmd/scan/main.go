af141870f4c0835ed84cb982cf3900d5fea4eb39
Nicolas Brun <nbrun@gld.be>
2024-05-08 20:49:00 +0200

    cleaning

--- cmd/scan/main.go
+++ cmd/scan/main.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/midbel/sweet"
+	"github.com/midbel/sweet/internal/lang"
 )
 
 func main() {
-	dialect := flag.String("d", "", "dialect")
 	flag.Parse()
 
 	r, err := os.Open(flag.Arg(0))
@@ -19,19 +18,19 @@ func main() {
 	}
 	defer r.Close()
 
-	scan, err := sweet.Scan(r, sweet.KeywordsForDialect(*dialect))
+	scan, err := lang.Scan(r, lang.GetKeywords())
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	for {
 		tok := scan.Scan()
-		if tok.Type == sweet.Invalid {
+		if tok.Type == lang.Invalid {
 			fmt.Fprintf(os.Stderr, "invalid token found at %s", tok.Position)
 			fmt.Fprintln(os.Stderr)
 			os.Exit(1)
 		}
-		if tok.Type == sweet.EOF {
+		if tok.Type == lang.EOF {
 			break
 		}
 		fmt.Println(tok.Position, tok)
